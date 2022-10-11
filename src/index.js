893050e676926bd5daa5eb4f49c7b8538f24e401
Fuzi18 <fuzesi.tamas@petrik.hu>
2022-10-11 12:25:39 +0200

    class

--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
+class Szazlabu{
+  #labakSzama;
+  constructor(labakSzama) {
+    this.#labakSzama = labakSzama;
+  }
+  toString() {
+    return labakSzama + "lába van";
+  }
+}
+
+
+let t = [];
+
 
 document.addEventListener('DOMContentLoaded', () => {
   let tomb = [];
-for (let i = 0; i < 30; i++) {
-    tomb.push(Math.floor(Math.random()* 30 + 1));
+ 
+  for (let i = 0; i < 30; i++) {
+  
+    tomb.push(Math.floor(Math.random() * 30 + 1));
   }
   tomb.forEach((e) => {
     console.log(e);
   })
    
 
+  
+});
+
+document.getElementById('otteloszthato').addEventListener('click', () => {
+  t.filter(e => e % 5 == 0);
   
 })
